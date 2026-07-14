e5713ed2c043982a7f5245840e5c0f3b0b568820
Niku Ghimre <nikughimire8@gmail.com>
2026-07-14 19:11:22 +0545

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,25 @@
+# Greet any User
+def good_Morning(user):
+    print("good Morning", user)
+good_Morning ("Niku")
+
+# Find the square of a number
+def square(n):
+    print(n*n)
+square (6)
+
+# Check if the number is odd or even
+def odd_even(n):
+    if n % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+odd_even (9)
+odd_even (3)
+odd_even(4)
+
+
+
+
+
+
